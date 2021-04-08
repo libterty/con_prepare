ea33d51bab452a79d27928fdd662345936247696
lib <dreak603@gmail.com>
2021-04-08 15:59:52 +0800

    Question: https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3701/

---
+++ Letter-Combinations-Phone-Number/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+  void dfs(const string& digits, const vector<vector<char>>& d, int l, string& cur, vector<string>& ans) {
+    if (l == digits.length()) {
+      ans.push_back(cur);
+      return;
+    }
+
+    for (const char c : d[digits[l] - '0']) {
+      cur.push_back(c);
+      dfs(digits, d, l + 1, cur, ans);
+      cur.pop_back();
+    }
+  }
+public:
+  vector<string> letterCombinations(string digits) {
+    if (digits.empty())
+      return {};
+    vector<vector<char>> d(10);
+    d[0] = {' '};
+    d[1] = {};
+    d[2] = {'a', 'b', 'c'};
+    d[3] = {'d', 'e', 'f'};
+    d[4] = {'g', 'h', 'i'};
+    d[5] = {'j', 'k', 'l'};
+    d[6] = {'m', 'n', 'o'};
+    d[7] = {'p', 'q', 'r', 's'};
+    d[8] = {'t', 'u', 'v'};
+    d[9] = {'w', 'x', 'y', 'z'};
+    vector<string> ans;
+    string cur;
+    dfs(digits, d, 0, cur, ans);
+    return ans;
+  }
+};
+
+int main() {
+  string d = "23";
+  Solution sol;
+  vector<string> ans = sol.letterCombinations(d);
+  for (int i = 0; i < (int)ans.size(); ++i)
+    cout << ans[i] << " ";
+  cout << endl;
+  return 0;
+}
